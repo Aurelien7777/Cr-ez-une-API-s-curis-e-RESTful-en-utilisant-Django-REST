c133707b082134541c81e418b9695839f87b8e3c
Aurélien Amorin <amorin77184@gmail.com>
2026-03-20 11:41:13 +0100

    Modification de users/views avec ajout d'un fonction get_permissions

--- users/views.py
+++ users/views.py
@@ -1,11 +1,15 @@
-from users.serializers import UserSerializer
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
+
 from .models import User
-from rest_framework.permissions import IsAuthenticated
+from .serializers import UserSerializer
+
 
 class UserViewSet(ModelViewSet):
-    
+    queryset = User.objects.all()
     serializer_class = UserSerializer
-    def get_queryset(self):
-        return User.objects.all()
 
+    def get_permissions(self):
+        if self.action == "create":
+            return [AllowAny()]
+        return [IsAuthenticated()]
