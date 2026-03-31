d3be490c88e2e05c5f456ece05a7c7713da29a0e
Aurélien Amorin <amorin77184@gmail.com>
2026-03-31 08:53:35 +0200

    Age rendu obligatoire avec la suppression des paramètres dans le model user pour l'attribut age

--- users/models.py
+++ users/models.py
@@ -2,6 +2,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-    age = models.PositiveIntegerField(null=True, blank=True)
+    age = models.PositiveIntegerField()
     can_be_contacted = models.BooleanField(default=False)
     can_data_be_shared = models.BooleanField(default=False)
